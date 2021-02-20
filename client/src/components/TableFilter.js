1a77187f1346fd704bdb296b1949b1fd2d3efa59
Daniel <kouznetsov1996@gmail.com>
2021-02-20 13:01:20 +0100

    h

---
+++ client/src/components/TableFilter.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from "react";
+import "./Components.css";
+
+class TableFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          width: "80%",
+          margin: "auto",
+        }}
+      >
+        <ProgramFilter />
+        <AreaFilter />
+        <PrecisionFilter />
+      </div>
+    );
+  }
+}
+
+function ProgramFilter() {
+  return (
+    <div className="filterBox">
+      <h3>ProgramFilter</h3>
+      <div class="col-4" style={{ display: "inline-block", width: "100%" }}>
+        <div class="list-group" id="list-tab" role="tablist">
+          <a
+            class="list-group-item list-group-item-action active"
+            id="list-home-list"
+            data-toggle="list"
+            href="#list-home"
+            role="tab"
+            aria-controls="home"
+          >
+            Datateknik
+          </a>
+          <a
+            class="list-group-item list-group-item-action"
+            id="list-profile-list"
+            data-toggle="list"
+            href="#list-profile"
+            role="tab"
+            aria-controls="profile"
+          >
+            Mjukvaruteknik
+          </a>
+          <a
+            class="list-group-item list-group-item-action"
+            id="list-messages-list"
+            data-toggle="list"
+            href="#list-messages"
+            role="tab"
+            aria-controls="messages"
+          >
+            DPU
+          </a>
+          <a
+            class="list-group-item list-group-item-action"
+            id="list-settings-list"
+            data-toggle="list"
+            href="#list-settings"
+            role="tab"
+            aria-controls="settings"
+          >
+            Indek
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function AreaFilter() {
+  return (
+    <div className="filterBox">
+      <h3>AreaFilter</h3>
+      <div
+        className="btn-group-vertical"
+        style={{ display: "inline-block", width: "100%" }}
+      >
+        <button
+          className="filterButton"
+          type="button"
+          class="btn btn-secondary"
+        >
+          Area 1
+        </button>
+        <button
+          className="filterButton"
+          type="button"
+          class="btn btn-secondary"
+        >
+          Area 2
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function PrecisionFilter() {
+  return (
+    <div className="filterBox">
+      <h3>PrecisionFilter</h3>
+    </div>
+  );
+}
+
+export default TableFilter;
